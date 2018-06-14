ac00975420e248a35487d8eb00e023981b238a92
Fabian Canas <fcanas@gmail.com>
2018-06-13 20:25:10 -0400

    Add a test for flatMap

--- Tests/FFCParserCombinatorTests/FFCParserCombinatorTests.swift
+++ Tests/FFCParserCombinatorTests/FFCParserCombinatorTests.swift
@@ -38,13 +38,23 @@ class FFCParserCombinatorTests: XCTestCase {
     }
 
     func testOr() {
-
         let alternativeParser = "One thing" <|> "Another"
 
         XCTAssertEqual(alternativeParser.run("One thing")?.0, "One thing")
         XCTAssertEqual(alternativeParser.run("Another")?.0, "Another")
         XCTAssertEqual(alternativeParser.run("And Another")?.0, nil)
         XCTAssertEqual(alternativeParser.run("One more")?.0, nil)
+    }
+
+    func testFlatMap() {
+        let lessThan5 = { (i: Int) in (i<5) ? i : nil }
+        let smallParser = lessThan5 <^!> Int.parser
+
+        XCTAssertEqual(smallParser.run("1")?.0, 1)
+        XCTAssertEqual(smallParser.run("4")?.0, 4)
+        XCTAssertEqual(smallParser.run("5")?.0, nil)
+        XCTAssertEqual(smallParser.run("-12")?.0, -12)
+        XCTAssertEqual(smallParser.run("😎")?.0, nil)
     }
 
     static var allTests = [
